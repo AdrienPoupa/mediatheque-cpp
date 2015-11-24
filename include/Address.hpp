9bd93f3b1840a7724602e91de1504bd3738a0a12
Edgar <edgarbuob@yahoo.fr>
2015-11-24 16:13:45 +0100

    class dress

--- include/Address.hpp
+++ include/Address.hpp
@@ -11,15 +11,39 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <string>
 
 class Address {
     std::string _country;
     unsigned int _houseNumber;
-    std::string _postalCode;
+    std::string _postalCode;// à cause de la corse et des départements commencant par 0
     std::string _streetName;
     std::string _town;
     
 public:
+    Address();
+    
+    std::string getCountry() const;
+    void setCountry(const std::string& pays );
+
+    int getHouseNumber() const;
+    void setHouseNumber(const int& numero);
+
+    std::string getPostalCode() const;
+    void setPostalCode(const std::string& codePostal);
+
+    std::string getTown() const;
+    void setTown(const std::string& ville);
+
+    std::string getStreetName() const;
+    void setStreetName(const std::string& rue );
+    
+    friend std::ostream& operator<< (std::ostream& stream, const Address& adresse);
+    friend std::istream& operator>> (std::istream& stream, Address& addresse);
+
+    
+    
+    
     
 };
 
--- src/Address.cpp
+++ src/Address.cpp
@@ -7,3 +7,96 @@
 //
 
 #include "Address.hpp"
+#include <iostream>
+
+using namespace std;
+
+Address::Address()
+{
+    
+}
+
+string Address::getCountry() const
+{
+    return _country;
+}
+
+void Address::setCountry(const string& pays)
+{
+    _country = pays;
+}
+
+int Address::getHouseNumber() const
+{
+    return _houseNumber;
+}
+
+void Address::setHouseNumber(const int &numero)
+{
+    _houseNumber = numero;
+}
+
+string Address::getPostalCode() const
+{
+    return _postalCode;
+}
+
+void Address::setPostalCode(const string &codePostal)
+{
+    _postalCode = codePostal;
+}
+
+string Address::getTown() const
+{
+    return _town ;
+}
+
+void Address::setTown(const string& ville)
+{
+    _town = ville ;
+}
+
+string Address::getStreetName() const
+{
+    return _streetName;
+}
+
+void Address::setStreetName(const string& rue)
+{
+    _streetName = rue;
+}
+
+ostream& operator<< (ostream& stream, const Address& adresse)
+{
+    stream << adresse._houseNumber << endl << adresse._streetName << endl << adresse._postalCode << endl << adresse._town << endl << adresse._country << endl ;
+    return stream;
+}
+
+istream& operator>> (std::istream& stream, Address& adresse)
+{
+    
+    cout<<"Saisie de l'adresse "<<endl;
+    
+    cout<<"Numero de rue : ";
+    stream >> adresse._houseNumber;
+    cout<<"Rue : ";
+    stream >> adresse._streetName;
+    cout<<"Code postal : ";
+    stream>> adresse._postalCode ;
+    cout <<"Ville : ";
+    stream>> adresse._town ;
+    cout << "Pays : ";
+    stream >> adresse._country ;
+    
+    return stream;
+    
+}
+
+
+
+
+
+
+
+
+
