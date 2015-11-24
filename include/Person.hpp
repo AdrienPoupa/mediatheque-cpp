add019284deaa44a4c4a5f011a6b5df1183eef90
AdrienPoupa <adrien@poupa.fr>
2015-11-24 15:00:21 +0100

    Class Person

--- include/Person.hpp
+++ include/Person.hpp
@@ -15,14 +15,24 @@
 #include "Date.hpp"
 
 class Person {
+private:
     unsigned int _id;
-    std::string _lastname;
+    std::string _lastName;
     std::string _firstName;
     Date _birthDate;
-    
-    
 public:
-    
+    Person(std::string lastName = "Inconnu", std::string firstName = "Inconnu");
+    Person(int id); // Get a person from an ID provided by DB
+    virtual ~Person();
+    virtual unsigned int getId();
+    virtual std::string getLastName();
+    virtual std::string getFirstName();
+    virtual Date getBirthDate();
+    virtual void setLastName(std::string lastName);
+    virtual void setFirstName(std::string firstName);
+    virtual void setBirthDate(Date birthDate);
+    virtual void addToDB();
+    virtual void updateDB();
 };
 
 #endif /* Person_hpp */
--- src/Person.cpp
+++ src/Person.cpp
@@ -7,3 +7,64 @@
 //
 
 #include "Person.hpp"
+
+Person::Person(std::string lastName, std::string firstName) : _lastName(lastName), _firstName(firstName)
+{
+    Date(1, 1, 1900);
+    _id = 0; // No ID for now
+}
+
+Person::Person(int id) // Get a person from an ID provided by DB
+{
+    // TODO: query to get an existing person
+}
+
+Person::~Person()
+{
+    // Nothing for now
+}
+
+unsigned int Person::getId()
+{
+    return _id;
+}
+
+std::string Person::getLastName()
+{
+    return _lastName;
+}
+
+std::string Person::getFirstName()
+{
+    return _firstName;
+}
+
+Date Person::getBirthDate()
+{
+    return _birthDate;
+}
+
+void Person::setLastName(std::string lastName)
+{
+    _lastName = lastName;
+}
+
+void Person::setFirstName(std::string firstName)
+{
+    _firstName = firstName;
+}
+
+void Person::setBirthDate(Date birthDate)
+{
+    _birthDate = birthDate;
+}
+
+void Person::addToDB()
+{
+    // TODO: query INSERT INTO
+}
+
+void Person::updateDB()
+{
+    // TODO: query INSERT INTO
+}
