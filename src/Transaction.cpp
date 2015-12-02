c32d143d08e1142d880528decc18bc5d2650e002
AdrienPoupa <adrien@poupa.fr>
2015-12-02 17:23:51 +0100

    Implement Transaction class

--- src/Transaction.cpp
+++ src/Transaction.cpp
@@ -8,12 +8,11 @@
 
 #include "Transaction.hpp"
 
-Transaction::Transaction(Article article, User user, Date beginning, Date finish) :
+Transaction::Transaction(Article *article, User user, Date beginning, Date finish) :
     _article(article), _user(user), _beginning(beginning), _finish(finish)
 {
 
 }
-
 
 Transaction::~Transaction()
 {
@@ -38,21 +37,50 @@ void Transaction::getTransactions(int day, int month, int year)
 /*
 Transaction Transaction::getTransaction(int id)
 {
-
+    // TODO
 }
 */
 
-bool Transaction::updateTransation()
+bool Transaction::save()
 {
+    // TODO
     return true;
 }
 
-bool Transaction::deleteTransaction()
+bool Transaction::remove()
 {
+    // If the genre doesn't exist yet, we can't remove it
+    if (_id == 0) {
+        return false;
+    }
+
+    try
+    {
+        SQLite::Database    dbTransaction("example.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
+         // Delete query
+        SQLite::Statement   query(dbTransaction, "DELETE FROM transactions WHERE id=?");
+        query.bind(1, (int) _id);
+        query.exec();
+
+        return true;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << "SQLite exception: " << e.what() << std::endl;
+        return false;
+    }
+
     return true;
 }
 
-bool Transaction::insertTransaction()
+std::ostream& operator<<(std::ostream& os, const Transaction& transaction)
 {
-    return true;
+    // TODO
+    return os;
+}
+
+std::istream& operator>>(std::istream& is, Transaction& transaction)
+{
+    // TODO
+    return is;
 }
