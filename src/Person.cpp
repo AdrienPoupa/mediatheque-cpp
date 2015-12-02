dde88067db3c6877478584126984d1fdb34667a0
AdrienPoupa <adrien@poupa.fr>
2015-12-02 17:24:00 +0100

    Better cin & cout

--- src/Person.cpp
+++ src/Person.cpp
@@ -72,7 +72,13 @@ ostream& operator<< (ostream& stream, const Person& person)
 
 istream& operator>> (istream& stream, Person& person)
 {
-    stream >> person._firstName >> person._lastName >> person._birthDate;
+    cout << "Saisie d'une personne" << endl;
+    cout << "Saisie du prenom" << endl;
+    stream >> person._firstName;
+    cout << "Saisie du nom" << endl;
+    stream >> person._lastName;
+    cout << "Saisie de l'anniversaire" << endl;
+    stream >> person._birthDate;
 
     return stream;
 }
