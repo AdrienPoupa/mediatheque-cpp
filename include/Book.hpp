1a9b95fa5b26f449cd169c3504436e6f0bbe0f96
AdrienPoupa <adrien@poupa.fr>
2015-12-02 17:23:35 +0100

    Implement Book class

--- include/Book.hpp
+++ include/Book.hpp
@@ -11,12 +11,26 @@
 
 #include <stdio.h>
 
+#include <string>
+
 #include "Article.hpp"
 
 class Book : public Article {
-    
+protected:
+    int _pages;
+    std::string _editor;
 public:
-    
+    Book();
+    ~Book();
+
+    int getPages() const;
+    void setPages(const int& pages);
+
+    std::string getEditor() const;
+    void setEditor(const std::string& editor);
+
+    bool save();
+    bool remove();
 };
 
 #endif /* Book_hpp */
