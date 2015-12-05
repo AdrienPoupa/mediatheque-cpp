eac07de884a79408af7d2fe3c3c68f33028690c0
AdrienPoupa <adrien@poupa.fr>
2015-12-05 11:47:10 +0100

    Update BaseModel

--- src/BaseModel.cpp
+++ src/BaseModel.cpp
@@ -15,7 +15,8 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
      */
     
     // Update
-    if (stoi(data["_id"][0]) != 0) {
+    if (stoi(data["id"][0]) != 0) {
+        //cout << "Updating ..." << endl;
         try
         {
             SQLite::Database db("example.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
@@ -23,7 +24,7 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
             string queryString = "UPDATE " + table + " SET ";
             
             for(auto const& elem : data){
-                if(elem.first != "_id"){
+                if(elem.first != "id"){
                     queryString += elem.first.c_str();
                     queryString += "=?, ";
                 }
@@ -31,7 +32,7 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
             
             queryString = queryString.substr(0, queryString.size() - 2);
             
-            queryString += "WHERE id=?;";
+            queryString += "WHERE id=" + data["id"][0] + ";";
             
             // queryString : UPDATE table SET attr1=?, attr2=?, attr3=? WHERE id=?
 
@@ -39,15 +40,16 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
             
             int n = 1;
             for(auto const& elem : data){
-                if(elem.first != "_id"){
+                if(elem.first != "id"){
                     if(elem.second[1] == "string"){
                         query.bind(n, elem.second[0]);
                     }
                     else if(elem.second[1] == "int"){
                         query.bind(n, stoi(elem.second[0]));
                     }
+                    n++;
                 }
-                n++;
+                
             }
             
             // queryString: UPDATE table SET attr1=val1, attr2=val2, attr3=val3 WHERE id=_idval
@@ -66,14 +68,26 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
     // Insert
     else
     {
+        //cout << "Inserting ..." << endl;
         try
         {
             SQLite::Database db("example.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
             
-            string queryString = "INSERT INTO " + table + " VALUES( ";
+            string queryString = "INSERT INTO " + table + "(";
             
-            for(auto const& elem : data){
-                if(elem.first != "_id"){
+            for(auto const& elem: data){
+                if(elem.first != "id"){
+                    queryString += elem.first + ", ";
+                }
+            }
+            
+            queryString = queryString.substr(0, queryString.size() - 2);
+            queryString += ") ";
+            
+            queryString += "VALUES( ";
+            
+            for(auto const& elem: data){
+                if(elem.first != "id"){
                     queryString += "?, ";
                 }
             }
@@ -87,17 +101,17 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
             
             int n = 1;
             for(auto const& elem : data){
-                if(elem.second[1] == "string"){
-                    query.bind(n, elem.second[0]);
+                if(elem.first != "id"){
+                    if(elem.second[1] == "string"){
+                        query.bind(n, elem.second[0]);
+                    }
+                    else if(elem.second[1] == "int"){
+                        query.bind(n, stoi(elem.second[0]));
+                    }
+                    n++;
                 }
-                else if(elem.second[1] == "int"){
-                    query.bind(n, stoi(elem.second[0]));
-                }
-                n++;
             }
-
             
-            // Insert query
             query.exec();
             
             // Update current ID
