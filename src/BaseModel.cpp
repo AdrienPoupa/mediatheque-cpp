16add2f064373ff18261f8bcbfeb5222afbf372b
AdrienPoupa <adrien.poupa@efrei.net>
2015-12-05 19:42:36 +0100

    Rename DB

--- src/BaseModel.cpp
+++ src/BaseModel.cpp
@@ -5,15 +5,15 @@
 using namespace std;
 
 map<string, string> BaseModel::getById(const string& table, const int& id){
-    
+
     map<string, string> data = map<string, string>();
-    
+
     try {
-        SQLite::Database db("example.db3");
-        
+        SQLite::Database db("mediatheque.db3");
+
         SQLite::Statement query(db, "SELECT * FROM " + table + " WHERE id=?");
         query.bind(1, id);
-        
+
         int resultCount = 0;
         while (query.executeStep())
         {
@@ -22,15 +22,15 @@ map<string, string> BaseModel::getById(const string& table, const int& id){
                 data.insert({query.getColumnName(i), query.getColumn(i).getText()});
             }
         }
-        
+
         /*
          for(auto const& elem: data){
          cout << elem.first << ":" << elem.second << endl;
          }
          */
-        
+
         return data;
-        
+
     } catch (std::exception& e) {
         std::cout << "exception: " << e.what() << std::endl;
         return data;
@@ -38,7 +38,7 @@ map<string, string> BaseModel::getById(const string& table, const int& id){
 }
 
 int BaseModel::save(const string& table, map<string, vector<string>> data){
-    
+
     /*
      data : {
         {"_id", {"15", "int"}},
@@ -46,31 +46,31 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
         ...
      }
      */
-    
+
     // Update
     if (stoi(data["id"][0]) != 0) {
         //cout << "Updating ..." << endl;
         try
         {
-            SQLite::Database db("example.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
-            
+            SQLite::Database db("mediatheque.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
+
             string queryString = "UPDATE " + table + " SET ";
-            
+
             for(auto const& elem : data){
                 if(elem.first != "id"){
                     queryString += elem.first.c_str();
                     queryString += "=?, ";
                 }
             }
-            
+
             queryString = queryString.substr(0, queryString.size() - 2);
-            
+
             queryString += "WHERE id=" + data["id"][0] + ";";
-            
+
             // queryString : UPDATE table SET attr1=?, attr2=?, attr3=? WHERE id=?
 
             SQLite::Statement query(db, queryString);
-            
+
             int n = 1;
             for(auto const& elem : data){
                 if(elem.first != "id"){
@@ -82,13 +82,13 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
                     }
                     n++;
                 }
-                
+
             }
-            
+
             // queryString: UPDATE table SET attr1=val1, attr2=val2, attr3=val3 WHERE id=_idval
-            
+
             query.exec();
-            
+
             return true;
         }
         catch (std::exception& e)
@@ -97,41 +97,41 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
             return false;
         }
     }
-    
+
     // Insert
     else
     {
         //cout << "Inserting ..." << endl;
         try
         {
-            SQLite::Database db("example.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
-            
+            SQLite::Database db("mediatheque.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
+
             string queryString = "INSERT INTO " + table + "(";
-            
+
             for(auto const& elem: data){
                 if(elem.first != "id"){
                     queryString += elem.first + ", ";
                 }
             }
-            
+
             queryString = queryString.substr(0, queryString.size() - 2);
             queryString += ") ";
-            
+
             queryString += "VALUES( ";
-            
+
             for(auto const& elem: data){
                 if(elem.first != "id"){
                     queryString += "?, ";
                 }
             }
-            
+
             queryString = queryString.substr(0, queryString.size()-2);
-            
+
             queryString += ");";
-            
+
             // Insert query
             SQLite::Statement query(db, queryString);
-            
+
             int n = 1;
             for(auto const& elem : data){
                 if(elem.first != "id"){
@@ -144,9 +144,9 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
                     n++;
                 }
             }
-            
+
             query.exec();
-            
+
             // Update current ID
             int tmp = db.execAndGet("SELECT last_insert_rowid();");
             return tmp;
@@ -160,20 +160,20 @@ int BaseModel::save(const string& table, map<string, vector<string>> data){
 }
 
 bool BaseModel::remove(const std::string& table, const int& id){
-    
-    // We cannot delete a non-existing user
+
+    // We cannot delete a non-existing field
     if(id == 0){
         return false;
     }
-    
+
     try
     {
-        SQLite::Database db("example.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
+        SQLite::Database db("mediatheque.db3", SQLITE_OPEN_READWRITE|SQLITE_OPEN_CREATE);
         // Delete query
         SQLite::Statement   query(db, "DELETE FROM " + table + " WHERE id=?");
         query.bind(1, (int) id);
         query.exec();
-        
+
         return true;
     }
     catch (std::exception& e)
