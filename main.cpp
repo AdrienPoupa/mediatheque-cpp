6174ccfadda3f61cfb97f63aed48753c95cc3c15
AdrienPoupa <adrien.poupa@efrei.net>
2015-12-13 19:29:14 +0100

    Load the Library

--- main.cpp
+++ main.cpp
@@ -3,18 +3,6 @@
 #include <cstdlib>
 #include <string>
 
-#include "sqlite/SQLiteCpp.h"
-#include "BaseModel.hpp"
-#include "Book.hpp"
-#include "Cd.hpp"
-#include "Dvd.hpp"
-#include "Date.hpp"
-#include "Address.hpp"
-#include "Artist.hpp"
-#include "User.hpp"
-#include "User.hpp"
-#include "Transaction.hpp"
-#include "sha256.h"
 #include "Library.hpp"
 
 using namespace std;
